4241de5103b21e4d877018fd9e77b170b43acbac
Christophe Willaert <christophe.willaert@pandora.be>
2017-11-04 18:37:53 +0100

    Added calc.py

---
+++ calc.py
@@ -0,0 +1,17 @@
+class calc():
+
+    def add(x,y):
+        return x+y
+
+
+    def subtract(x,y):
+        pass
+
+    def divide(x,y):
+        pass
+
+    def square(x):
+        pass
+
+
+print(calc.add(10,2))
